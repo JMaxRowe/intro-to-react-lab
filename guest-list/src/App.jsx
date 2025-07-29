8d4833d7fb1531a74244735ef4f2df3b15bf9c40
JMaxRowe <jmaxrowe@gmail.com>
2025-07-29 15:22:38 +0100

    displays list and shames you if you have no friends

---
+++ guest-list/src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+const guests = [
+  // { name: "Alice", rsvp: true },
+  // { name: "Bob", rsvp: false },
+  // { name: "Charlie", rsvp: true }
+];
+
+const guestList = guests.map((guest, index) => (
+  <li key={index}>
+    {guest.name} - {guest.rsvp ? '✅ Attending' : '❌ Not attending'}
+  </li>
+))
+
+  return (
+    <>
+      <h1>{guests.length > 0 ? 'Guest List:' : 'You have no friends :('}</h1>
+      <ul>{guests.length > 0 ? guestList : <p>Feels bad</p>}</ul>
+    </>
+  )
+}
+
+export default App
